8b9855a25a41510b342e284a3f8f57dcd3598dbc
Diego Munoz <diego0433hd@gmail.com>
2023-12-17 20:18:42 -0300

    Agregando Nueva Pages, Soluciones

---
+++ src/Pages/NuestrasSoluciones.tsx
@@ -0,0 +1,9 @@
+const NuestrasSoluciones = () => {
+    return(
+        <>
+            <div className="grid">
+                <h1>Nuestras Soluciones</h1>
+            </div>
+        </>
+    )
+}
